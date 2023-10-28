50b4f575c5c77f9a4816a5942b3ac1a6fc13325c
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-10-28 11:51:06 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import "./ui/global.css"
+import { Inter, Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
